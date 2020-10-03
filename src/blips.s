fb0a0007f299eaccb82e9d48db88b608d1234fe9
Stelele <giftmugweni@gmail.com>
2020-10-03 21:35:10 +0100

    completed blips function minus skipping hold if negative blip detected

--- src/blips.s
+++ src/blips.s
@@ -18,38 +18,75 @@ blips:
     @ assume we are using a store before call calling convention
     @ r0 = signed char* samp
 
-    @calculate average of samp
+    @ calculate average of samp
     mov     r1, #0          @ r1 = 0, counter variable
     mov     r2, #0          @ r2 = 0, accumulator to store sum
 
+    @ first get sum of all the absolute value of all values in samp_1
     fLoop:
-        cmp     r1, #256    @ check if r1 == 256
-        beq     endFLoop    @ go to 
+        cmp     r1, #256    @ r1 -= 256
+        beq     endFLoop    @ if r1 == 256 go to endFLoop 
 
-        ldr     r3, [r0, r1]    @ r3 = samp[r1] get byte at r0 offset by r1
+        ldrsb     r3, [r0, r1]    @ r3 = samp[r1] get byte at r0 offset by r1
 
-        cmp     r3, #0      @ check if value is negative or positive
-        blt     negativeNum @ go to negativeNum
+        cmp     r3, #0          @ r3 -= 0
+        blt     negativeNum     @ if r3 < 0 go to neativeNum
 
-            add     r2, r2, r3  @ r2 = r2 + r3
             b       continue    @ go to continue          
 
         negativeNum:
-            
+            rsb     r3, r3, #0  @ r3 = 0 - r3 
 
         continue:
 
-        add     r2, r2, r3  @ r2 += r3
+        add     r2, r2, r3      @ r2 += r3
 
-        add     r1, r1, 1   @ r1 += 1
-        b       fLoop       @ go to fLoop    
+        add     r1, r1, #1      @ r1 += 1
+        b       fLoop           @ go to fLoop    
 
     endFLoop:
 
+    @ calculate average
+    mov     r2, r2, lsr 8   @ r2 = r2/256
+
+    @ calculate threshold
+    mov     r3, #3          @ r3 = 3
+    mul     r3, r3, r2      @ r3 *= r2 threshold value
+    
+    @ calculate blip threshold
+  
+    @ count number of positive blips
+    mov     r1, #0          @ r1 = 0, counter variable
+    mov     r4, #0          @ r4 = 0, accumulator storing number of positive numbers
+    mov     r6, #0          @ r6 = 0, hold variable 
+    fLoop2:
+        cmp     r1, #255        @ r1 -= 256
+        bgt     endFloop2       @ if r1 > 255 go to endFloop2
+
+        cmp     r6, #0          @ r6 -= 0
+        beq     noHold          @ if r6 == 0 go to noHold
+
+            sub     r6, r6, #1      @ r6 -= 1
+            b       continue2       @ go to continue2
+
+        @ if r6 == 0 do these operations
+        noHold:
+            ldrsb   r5, [r0, r1]    @ r5 = samp[r1]
+
+            cmp     r5, r3          @ r5 -= r3
+            blt     continue2       @ if r5 < r3 go to continue2
+
+                add     r4, r4, 1   @ r4 += 1
+                mov     r6, #2      @ r6 = 2
+
+            continue2:
+            add     r1, r1, #1      @ r1 += 1
+            b       fLoop2          @ go to fLoop2
+
+    endFloop2:
 
     @ function epilogue
     ret_out:
-    ldrb         r1, [r0,252]
-    mov         r0, r1              @ return out pointer
+    mov         r0, r4              @ return out pointer
     ldmea       fp, {fp, sp, pc}    @ do return call
     bx          lr                  @ return to calling function
--- src/main.c
+++ src/main.c
@@ -6,7 +6,31 @@ int main(){
     int numf;
     printf("Welcome to cosmic particle detector ver 0.1\n");
     printf("Testing sample block samp_1\n");
-    printf("%d\n",samp_1[10]);
+
+    int sum = 0;
+    for(int i = 0; i < 256; i ++){
+        if(samp_1[i] < 0)
+            sum += -1 * samp_1[i];
+        else
+            sum += samp_1[i];
+    }
+
+    int avg = sum/256;
+    int threshold = avg * 3;
+    int countedBlips = 0;
+    int hold = 0;
+    for(int i =0; i < 256; i++){
+        if(hold > 0){
+            hold -= 1;
+            continue;
+        }
+        if(samp_1[i] > threshold){
+            countedBlips++;
+            hold = 2;
+        }
+    }
+
+    printf("value is %d\n", countedBlips);
     numf = blips(samp_1);
 
     printf("Number blips = %d\n", numf);
